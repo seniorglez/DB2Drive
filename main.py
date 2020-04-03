1ca38c73abd3f38d44c50679dc734d58be881227
Diego Domínguez González <d.dominguez.glez1111@gmail.com>
2020-04-03 13:43:19 +0200

    Added basic methods

---
+++ main.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import httplib2
+import os, io
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+import auth
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+
+def uploadFile(filename,filepath,mimetype):
+    file_metadata = {'name': filename}
+    media = MediaFileUpload(filepath,
+                            mimetype=mimetype)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
